3f0598119d367585b4000f4ef8f6d2488d759119
whz <1152957995@qq.com>
2022-06-24 21:59:32 +0800

    2022年 6月24日 星期五 21时59分32秒 CST

--- machinePriceSpider/price-spider/access.py
+++ machinePriceSpider/price-spider/access.py
@@ -64,7 +64,7 @@ def init_user():
 def update_token(index):
 	userInfo = user[index]
 	logout(userInfo['token'])
-	time.sleep(5)
+	time.sleep(10)
 	print(time.strftime('%H:%M:%S'),'重新登录')
 	resp = login(userInfo["chromosome"], userInfo["body"])
 	if resp == -1:
