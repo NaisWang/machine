754c05d1f8d220f0629bb8a25588f223938b1013
whz <1152957995@qq.com>
2022-06-25 00:11:57 +0800

    2022年 6月25日 星期六 00时11分57秒 CST

--- machinePriceSpider/price-spider/login.py
+++ machinePriceSpider/price-spider/login.py
@@ -63,7 +63,7 @@ def login(chromosome, body):
 		try:
 			#return "08dcef810468f5cf0d695c591a42dcf5"
 			#response = requests.request("POST", url, headers=headers, data=payload)
-			resp = json.loads(requests.post(loginURL, headers=headers, data=str(data)).text)
+			resp = json.loads(requests.post(loginURL, headers=headers, data=str(body)).text)
 			print(resp)
 			if 'data' in resp and "accessToken" in resp["data"]:
 				log.log_error.append("登录成功, token为:" + str(resp["data"]["accessToken"]))
