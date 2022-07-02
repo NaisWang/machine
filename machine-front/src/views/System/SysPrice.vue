b927b682703a12d372b76fde79a529e2ceb28fa7
whz <1152957995@qq.com>
2022-07-02 19:50:04 +0800

    a

--- machine-front/src/views/System/SysPrice.vue
+++ machine-front/src/views/System/SysPrice.vue
@@ -192,8 +192,8 @@ export default {
       this.stopTimer = true;
       this.log = []
       let that = this;
-      document.getElementById("player").play()
-      $("#player").play()
+      let player = document.getElementById("player")
+      //$("#player").play()
       setTimeout(function fn() {
         if (that.stopTimer) {
           try {
@@ -201,6 +201,7 @@ export default {
               //       $http.get("http://127.0.0.1:5000/log").then(resp => {
               that.log = resp.data
               //if (that.log.autoCode === 1) {
+              player.play()
               //}
               setTimeout(fn, 1000);
             }).catch((e) => {
