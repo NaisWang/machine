2066820da647a5268a35ee398189c4361655882d
whz <1152957995@qq.com>
2022-07-02 22:02:34 +0800

    a

--- machine-front/src/utils/global.js
+++ machine-front/src/utils/global.js
@@ -1,5 +1,5 @@
 export default {
   //BASE_URL: "http://120.79.45.0:8081",
-  //BASE_URL: "http://120.79.195.87:8081",
-  BASE_URL: "http://127.0.0.1:8081",
+  BASE_URL: "http://120.79.195.87:8081",
+  //BASE_URL: "http://127.0.0.1:8081",
 }
